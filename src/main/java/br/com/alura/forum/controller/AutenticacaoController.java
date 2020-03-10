3767af9f977cd27edfb9a81a4c506c801f4462c7
Martin Petersen <petersen.martin.imd@gmail.com>
2020-03-10 20:18:58 -0300

    AUTENTICAÇÃO JWT

---
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,40 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.config.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm loginForm) {
+        UsernamePasswordAuthenticationToken dadosDeLogin = loginForm.convert();
+
+        try {
+            Authentication authentication = authManager.authenticate(dadosDeLogin);
+            String token = tokenService.gerarToken(authentication);
+            System.out.println(token);
+            return ResponseEntity.ok().build();
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
